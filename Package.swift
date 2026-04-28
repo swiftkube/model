2b8155aa408d4725968ccdd8b70a5ba63aeff8a3
Tobias <tobias.haeberle@me.com>
2026-04-28 22:47:30 +0200

    bump swift tools version to 5.7 and bump platform requirements

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
 	name: "SwiftkubeModel",
 	platforms: [
-		.macOS(.v10_13), .iOS(.v12), .tvOS(.v12), .watchOS(.v5)
+		.macOS(.v13), .iOS(.v16), .tvOS(.v16), .watchOS(.v9)
 	],
 	products: [
 		.library(
