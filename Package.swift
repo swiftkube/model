6a26283b2097b91f670ab5827b9f04216193243d
Iskandar Abudiab <mail@iabudiab.dev>
2026-05-05 00:38:36 +0200

    Use Swift Tools Version 6.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -27,5 +27,5 @@ let package = Package(
 				"SwiftkubeModel"
 		]),
 	],
-	swiftLanguageVersions: [.v5, .version("6")]
+	swiftLanguageModes: [.v6]
 )
